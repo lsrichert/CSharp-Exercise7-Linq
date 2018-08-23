ceb45576fbd71bbdf903693664170a69db008b51
Lauren Richert <lsrichert@gmail.com>
2018-08-23 10:57:06 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace exercise7Linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
